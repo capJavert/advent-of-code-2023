55f8617afbec37823c316050c4962f39cb70e273
capJavert <ante@kickass.website>
2023-12-02 21:17:18 +0100

    init

---
+++ utils/index.js
@@ -0,0 +1,25 @@
+import fs from 'fs/promises'
+import path from 'path'
+
+export const fetchInput = async ({ day, year }) => {
+    const fileName = path.join(process.cwd(), `inputs/input-${day}-${year}.txt`)
+
+    try {
+        return await fs.readFile(fileName, { encoding: 'utf-8' })
+    } catch {
+        console.log('no local input, fetching')
+
+        const headers = new Headers()
+        headers.append('cookie', `session=${process.env.AOC_SESSION}`)
+
+        const response = await fetch(`https://adventofcode.com/${year}/day/${day}/input`, {
+            method: 'GET',
+            headers: headers
+        })
+        const result = await response.text()
+
+        await fs.writeFile(fileName, result, { encoding: 'utf-8' })
+
+        return result
+    }
+}
