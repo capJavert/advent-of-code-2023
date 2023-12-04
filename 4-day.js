4bbcd4c5efb8602246bc5cb1f9154078c3d411f7
capJavert <ante@kickass.website>
2023-12-04 22:08:39 +0100

    feat(day-4): part 2

--- 4-day.js
+++ 4-day.js
@@ -3,11 +3,14 @@ import { fetchInput, splitByLines } from './utils/index.js'
 const main = async () => {
     const data = await fetchInput({ day: 4, year: 2023 })
 
-    const cards = splitByLines({ input: data }).map(item => {
+    const startingCards = splitByLines({ input: data }).reduce((acc, item, index) => {
         const [, items] = item.split(':')
 
+        const id = index + 1
         const [winningNumbers, numbers] = items.split('|')
-        return {
+
+        const card = {
+            id,
             numbers: numbers
                 .split(' ')
                 .filter(Boolean)
@@ -17,16 +20,49 @@ const main = async () => {
                 .filter(Boolean)
                 .map(item => +item)
         }
+        const matches = card.numbers.filter(number => card.winningNumbers.includes(number))
+
+        return {
+            ...acc,
+            [id]: {
+                ...card,
+                matches
+            }
+        }
+    }, {})
+
+    Object.values(startingCards).forEach(card => {
+        card.newCards = []
+
+        card.matches.forEach((match, index) => {
+            card.newCards.push(startingCards[card.id + index + 1])
+        })
     })
 
-    const points = cards.reduce((acc, card) => {
-        const matches = card.numbers.filter(number => card.winningNumbers.includes(number))
-        const cardPoints = matches.length ? 2 ** (matches.length - 1) : 0
+    let cards = Object.values(startingCards)
+    let cardsCount = cards.length
 
-        return acc + cardPoints
-    }, 0)
+    while (true) {
+        let didMatch = false
+        let newCards = []
 
-    console.log(points)
+        cards.forEach(card => {
+            cardsCount += card.newCards.length
+            newCards.push(...card.newCards)
+
+            if (card.newCards.length) {
+                didMatch = true
+            }
+        })
+
+        if (!didMatch) {
+            break
+        }
+
+        cards = newCards
+    }
+
+    console.log(cardsCount)
 }
 
 main()
