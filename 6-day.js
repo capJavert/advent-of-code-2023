dfe3c1fc081d8f2bb5363b31d66c475b4eaf94f4
capJavert <ante@kickass.website>
2023-12-06 18:18:34 +0100

    feat(day-6): part 2

--- 6-day.js
+++ 6-day.js
@@ -3,30 +3,23 @@ import { fetchInput, splitByLines } from './utils/index.js'
 const main = async () => {
     const data = await fetchInput({ day: 6, year: 2023 })
 
-    const [times, distances] = splitByLines({ input: data }).map(item => {
-        return item
+    const [time, distance] = splitByLines({ input: data }).map(item => {
+        return +item
             .split(' ')
             .map(item => +item)
             .filter(Boolean)
+            .join('')
     })
 
-    let winningStrategies = 1
+    let winningStrategies = 0
 
-    for (let i = 0; i < times.length; i++) {
-        const time = times[i]
-        const distance = distances[i]
-        let winningStrategiesCount = 0
+    for (let j = 0; j <= time; j++) {
+        const remainingTime = time - j
+        const traveledDistance = j * remainingTime
 
-        for (let j = 0; j <= time; j++) {
-            const remainingTime = time - j
-            const traveledDistance = j * remainingTime
-
-            if (traveledDistance > distance) {
-                winningStrategiesCount += 1
-            }
+        if (traveledDistance > distance) {
+            winningStrategies += 1
         }
-
-        winningStrategies *= winningStrategiesCount
     }
 
     console.log(winningStrategies)
