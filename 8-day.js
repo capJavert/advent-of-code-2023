3d04bbf65ad135af59aa358cef118a6898f3ca28
capJavert <ante@kickass.website>
2023-12-08 21:49:44 +0100

    feat(day-8): part 2

--- 8-day.js
+++ 8-day.js
@@ -6,31 +6,58 @@ const main = async () => {
     const [firstLine, ...lines] = splitByLines({ input: data })
     const instructions = firstLine.split('')
     const maps = lines.reduce((acc, line) => {
-        const [from, left, right] = line.match(/[A-Z]{3}/g)
+        const [from, left, right] = line.match(/[A-Z0-9]{3}/g)
 
         acc[from] = {
             L: left,
-            R: right
+            R: right,
+            isEnd: from.endsWith('Z')
         }
 
         return acc
     }, {})
 
-    const start = 'AAA'
-    const end = 'ZZZ'
-    let currentLocation = start
-    let index = 0
-    let steps = 0
+    const startingNodes = Object.keys(maps).filter(key => key.endsWith('A'))
+    const nodes = startingNodes.map(key => ({
+        start: key,
+        currentLocation: key
+    }))
 
-    while (currentLocation !== end) {
-        const instruction = instructions[index]
-        currentLocation = maps[currentLocation][instruction]
+    let allSteps = []
 
-        index = (index + 1) % instructions.length
-        steps += 1
+    for (let node of nodes) {
+        let currentLocation = node.currentLocation
+        let index = 0
+        let steps = 0
+
+        while (true) {
+            const instruction = instructions[index]
+            currentLocation = maps[currentLocation][instruction]
+            const { isEnd } = maps[currentLocation]
+
+            steps += 1
+
+            if (isEnd) {
+                break
+            }
+
+            index = (index + 1) % instructions.length
+        }
+
+        allSteps.push(steps)
     }
 
-    console.log(steps)
+    const gcd = (a, b) => {
+        if (b === 0) {
+            return a
+        }
+
+        return gcd(b, a % b)
+    }
+
+    const lcm = (a, b) => Math.abs(a * b) / gcd(a, b)
+
+    console.log(allSteps.reduce(lcm))
 }
 
 main()
