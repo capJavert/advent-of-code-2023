009c314eafedff7c52447fd5a67f2ce1532f3e58
capJavert <ante@kickass.website>
2023-12-13 09:38:22 +0100

    feat(day-13): part 2

--- 13-day.js
+++ 13-day.js
@@ -27,7 +27,7 @@ const main = async () => {
             }
         })
 
-    const findReflecion = items => {
+    const findReflecion = (items, smudgedReflection) => {
         for (let i = 0; i < items.length; i++) {
             const item = items[i]
             const next = items[i + 1]
@@ -39,6 +39,10 @@ const main = async () => {
             if (item === next) {
                 for (let j = 0; j < items.length; j++) {
                     if (!items[i - j] || !items[i + 1 + j]) {
+                        if (smudgedReflection && smudgedReflection === i + 1) {
+                            break
+                        }
+
                         return i + 1
                     }
 
@@ -55,14 +59,38 @@ const main = async () => {
     let rowsCount = 0
     let columnsCount = 0
 
-    patterns.forEach(({ rows, columns, raw }) => {
-        let reflection = findReflecion(rows)
+    patterns.forEach(({ rows, columns }) => {
+        let smudgedType = 'rows'
+        let smudgedReflection = findReflecion(rows)
 
-        if (!reflection) {
-            reflection = findReflecion(columns)
-            columnsCount += reflection
-        } else {
-            rowsCount += reflection
+        if (!smudgedReflection) {
+            smudgedReflection = findReflecion(columns)
+            smudgedType = 'columns'
+        }
+
+        for (let i = 0; i < rows.length; i++) {
+            for (let j = 0; j < rows[0].length; j++) {
+                const newRows = [...rows]
+                const newColumns = [...columns]
+
+                const smudge = newRows[i][j] === '#' ? '.' : '#'
+                newRows[i] = newRows[i].substring(0, j) + smudge + newRows[i].substring(j + 1)
+                newColumns[j] = newColumns[j].substring(0, i) + smudge + newColumns[j].substring(i + 1)
+
+                let reflection = findReflecion(newRows, smudgedType === 'rows' ? smudgedReflection : undefined)
+
+                if (!reflection) {
+                    reflection = findReflecion(newColumns, smudgedType === 'columns' ? smudgedReflection : undefined)
+
+                    if (reflection) {
+                        columnsCount += reflection
+                        return
+                    }
+                } else if (reflection) {
+                    rowsCount += reflection
+                    return
+                }
+            }
         }
     })
 
